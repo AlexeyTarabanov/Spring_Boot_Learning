e78d28f7a901ef894f39c83c851fb07945288e27
Alexey Tarabanov <tarabanov@bk.ru>
2021-05-01 20:44:19 +0300

    1. simple web application (mustache)

--- src/main/java/org/example/sweater/GreetingController.java
+++ src/main/java/org/example/sweater/GreetingController.java
@@ -1,13 +1,14 @@
 package org.example.sweater;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 /**
  Controller - програмный модуль, который по этому пути ("/greeting")
- слушает запросы от пользователя и возвращает какие-то данные, в нашем случае файл шаблона (greeting.html)
+ слушает запросы от пользователя и возвращает какие-то данные, в нашем случае файл шаблона (greeting.mustache)
  */
 
 @Controller
@@ -19,11 +20,17 @@ public class GreetingController  {
     public String greeting(
             @RequestParam(name = "name", required = false, defaultValue = "World") String name,
             // model - это то куда мы будем складывать данные, которые мы хотим вернуть пользователю
-            Model model
+            Map<String, Object> model
     ) {
         // в поле "name" ложим то, что мы получили из этого name-a
-        model.addAttribute("name", name);
+        model.put("name", name);
         // возвращаем имя файла спринговому контейнеру, который мы хотим отобразить
         return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model ) {
+        model.put("some", "Hello, letsCode!");
+        return "main";
     }
 }
